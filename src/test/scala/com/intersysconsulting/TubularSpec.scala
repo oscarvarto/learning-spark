5b4246af41e0b63b137bd25ec3abd666e35f746e
Oscar Vargas Torres <oscarvarto@protonmail.com>
2018-11-13 15:49:15 -0600

    Compiles, but test fails

---
+++ src/test/scala/com/intersysconsulting/TubularSpec.scala
@@ -0,0 +1,46 @@
+package com.intersysconsulting
+
+import org.scalatest.FunSpec
+
+import org.apache.spark.sql.types.StringType
+
+import com.github.mrpowers.spark.fast.tests.DataFrameComparer
+import com.github.mrpowers.spark.daria.sql.SparkSessionExt._
+
+class TubularSpec
+  extends FunSpec
+    with SparkSessionWrapper
+    with DataFrameComparer {
+
+  describe("withGoodVibes") {
+
+    it("appends a chi column to a DataFrame") {
+
+      val sourceDF = spark.createDF(
+        List(
+          "sue",
+          "fan"
+        ), List(
+          ("name", StringType, true)
+        )
+      )
+
+      val actualDF = sourceDF.transform(Tubular.withGoodVibes())
+
+      val expectedDF = spark.createDF(
+        List(
+          ("sue", "happy"),
+          ("fan", "happy")
+        ), List(
+          ("name", StringType, true),
+          ("chi", StringType, false)
+        )
+      )
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
+    }
+
+  }
+
+}
